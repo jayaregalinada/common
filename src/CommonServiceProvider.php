d3f9525204c8a93ffa6dbda0da73f582aa3c4cf8
Jay Are Galinada <jayaregalinada@gmail.com>
2015-07-18 08:00:43 +0800

    Fixed the Service Provider - add CACHENAME - $defer to false [IMPORTANT] - Add Docs - Include timestamp in migration

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -2,10 +2,58 @@
 
 namespace Jag\Common;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class CommonServiceProvider extends ServiceProvider
 {
+
+    /**
+     * @var string
+     */
+    const CACHENAME = 'jag\common::migrationName';
+
+    /**
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Array of other providers
+     *
+     * @var array
+     */
+    protected $otherProviders = [
+        'Laravel\Socialite\SocialiteServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+        'Intervention\Image\ImageServiceProvider',
+        'Illuminate\Html\HtmlServiceProvider',
+        'Orangehill\Iseed\IseedServiceProvider',
+        'Clockwork\Support\Laravel\ClockworkServiceProvider',
+        'Zizaco\Entrust\EntrustServiceProvider',
+        'JeroenG\Packager\PackagerServiceProvider',
+    ];
+
+    /**
+     * @return string
+     */
+    protected function getMigrationName()
+    {
+        return 'common_create_users_table';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTimestampMigrationName()
+    {
+        if(!Cache::has(static::CACHENAME)) {
+            Cache::forever(static::CACHENAME, date('Y_m_d_His') . '_' . $this->getMigrationName());
+        }
+
+        return Cache::get(static::CACHENAME);
+    }
+
     /**
      * Perform post-registration booting of services.
      *
@@ -13,7 +61,9 @@ class CommonServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__. '/../database/migrations/'. $this->getMigrationName() .'.php' => database_path('migrations/' . $this->getTimestampMigrationName() .'.php')
+        ], 'migrations');
     }
 
     /**
@@ -26,5 +76,19 @@ class CommonServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__. '/../config/app.php', 'app'
         );
+        $this->registerOtherProviders();
     }
+
+    /**
+     * Register the other providers
+     *
+     * @return void
+     */
+    protected function registerOtherProviders()
+    {
+        foreach ($this->otherProviders as $key => $value) {
+            $this->app->register($value);
+        }
+    }
+
 }
