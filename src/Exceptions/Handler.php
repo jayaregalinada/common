85fe39de3d3d5dd17e888ec7618f65b0fa2dff8a
Jay Are Galinada <jayaregalinada@gmail.com>
2015-08-11 12:43:55 +0800

    Fixed `getJsonResponse()` because `parent::render()` do not return an instance of `Illuminate\Http\Response`

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -3,16 +3,14 @@
 namespace Jag\Common\Exceptions;
 
 use Exception;
-use Whoops\Run;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Handler\JsonResponseHandler;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class Handler extends ExceptionHandler
 {
-
     /**
      * Render an exception into an HTTP response.
      *
@@ -28,8 +26,7 @@ class Handler extends ExceptionHandler
         } else {
             $response = parent::render($request, $e);
             if ($request->ajax() || $request->wantsJson()) {
-                return response()->jsonp($request->input('callback'), $this->getJsonResponse($request, $response),
-                    $response->getStatusCode());
+                return response()->jsonp($request->input('callback'), $this->getJsonResponse($request, $e, $response), $response->getStatusCode());
             } else {
                 return $response;
             }
@@ -44,7 +41,7 @@ class Handler extends ExceptionHandler
      */
     public function renderExceptionWithWhoops($request, Exception $e)
     {
-        $whoops = new Run;
+        $whoops = new Run();
         if ($request->ajax() || $request->wantsJson()) {
             $whoops->pushHandler(new JsonResponseHandler);
         } else {
@@ -56,22 +53,23 @@ class Handler extends ExceptionHandler
 
     /**
      * @param \Illuminate\Http\Request  $request
-     * @param \Illuminate\Http\Response $response
+     * @param \Exception                $exception
+     * @param $response
      *
      * @return array
      */
-    protected function getJsonResponse(Request $request, Response $response)
+    protected function getJsonResponse(Request $request, Exception $exception, $response)
     {
-        $o = new \stdClass;
+        $o = new \stdClass();
         $o->code = $response->getStatusCode();
         $o->title = $response::$statusTexts[$response->getStatusCode()];
-        $o->message = $response->exception->getMessage();
+        $o->message = $exception->getMessage();
         if ($request->input('with') == 'exception') {
             $o->exception = [
-                'file'       => class_basename($response->exception->getFile()),
-                'line'       => $response->exception->getLine(),
-                'code'       => $response->exception->getCode(),
-                'instanceof' => class_basename($response->exception),
+                'file'       => class_basename($exception->getFile()),
+                'line'       => $exception->getLine(),
+                'code'       => $exception->getCode(),
+                'instanceof' => class_basename($exception),
             ];
         }
 
